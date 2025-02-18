61c970aeb5b763f96da7882f878ddec5167ddfa6
misha dyshkant <mishadyshkant12@gmail.com>
2025-02-18 23:14:07 +0200

    dasd

---
+++ models/prodectModel.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING
+  }
+}, {
+  timestamps: false
+});
+
+module.exports = Product;
