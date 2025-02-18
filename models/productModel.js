a348a2a7d079b3f84a1065e22b749d240557089f
misha dyshkant <mishadyshkant12@gmail.com>
2025-02-18 23:30:39 +0200

    third

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/__mocks__/db.js');
 
 const Product = sequelize.define('Product', {
   id: {
