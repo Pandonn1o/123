e5b978ec90fbeeab11f2fc6e867c06f3f98ebcb3
misha dyshkant <mishadyshkant12@gmail.com>
2025-02-18 23:57:11 +0200

    dsjhadklj

--- __test__/productsController.test.js
+++ __test__/productsController.test.js
@@ -1,3 +1,4 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
 const productsController = require("../controllers/productsController.js");
 jest.mock("../config/__mocks__/db.js");
 const app = require("../app");
@@ -29,7 +30,7 @@ describe("productsController", () => {
       price: 14.44,
       description: "Product B is a high-quality item.",
     });
-    await Product.create[products[0]];
+    await Product.create(products[0]);
     products[0].id = 1;
     await Product.create(products[1]);
     products[1].id = 2;
